cc0f5cb234d973e04a63854923924ae554919e40
kosarrezaei1 <rojinrezaei65@gmail.com>
2026-05-27 09:57:48 +0100

    Delete javascript1.js

--- javascript1.js
+++
@@ -1,9 +0,0 @@
-(document).on("Scroll", function () {
-    if
-        ($(document).scrollTop() > 50) {
-  $("header").addclass("shrink");
-    }
-    else {
-  $("header").removeclass("shrink");
-    }
-  });
